4bc118a914e044d3221b2c8b94764a235480e02d
xuxin-to-future <51843322+xuxin-to-future@users.noreply.github.com>
2020-03-23 10:56:56 +0800

    栈

--- stack.c
+++ stack.c
@@ -1,51 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct Stack {
     int *data;
-    int max_size, top;
-}Stack;
+    int top, size;
+} Stack;
 
-void init(Stack *s, int size){
-    s->data = (int *)malloc(sizeof(int) * size);
-    s->max_size = size;
+Stack *init(int n) {
+    Stack *s = (Stack *)malloc(sizeof(Stack));
+    s->data = (int *)malloc(sizeof(int) * n);
     s->top = -1;
+    s->size = n;
+    return s;
 }
 
-int push(Stack *s, int val){
-    if(s->top >= s->max_size - 1){
-        return 0;
-    }
-    s->top++;
-    s->data[s->top] = val;
-    return 1;
+int empty(Stack *s) {
+    return s->top == -1;
 }
 
-int pop(Stack *s){
-    if(s->top < 0){
-        return 0;
-    }
-    s->top--;
-    return 1;
-}
-
-int get_top(Stack *s){
+int top(Stack *s) {
     return s->data[s->top];
 }
 
-int empty(Stack *s){
-    if(s->top < 0){
-        return 1;
+int expand(Stack *s) {
+    int extr_size = s->size;
+    int *p;
+    while (extr_size) {
+        p = (int *)realloc(s->data, sizeof(int) * (s->size + extr_size));
+        if (p) break;
+        extr_size /= 2;
     }
-    return 0;
+    if (p == NULL) return 0;
+    s->size += extr_size;
+    s->data = p;
+    return 1;
 }
 
-void clear(Stack *s){
+int push(Stack *s, int val) {
+    if (s == NULL) return 0;
+    if (s->top + 1 == s->size) {
+        if (!expand(s)) return 0;
+        printf("expand successful! size = %d\n", s->size);
+    }
+    s->data[++(s->top)] = val;
+    return 1;
+}
+
+int pop(Stack *s) {
+    if (s == NULL) return 0;
+    if (empty(s)) return 0;
+    s->top -= 1;
+    return 1;
+}
+
+void output(Stack *s) {
+    for (int i = 0; i <= s->top; i++) {
+        printf("%d ", s->data[i]);
+    }
+    printf("\n");
+    return ;
+}
+
+void clear(Stack *s) {
+    if (s == NULL) return ;
     free(s->data);
     free(s);
+    return ;
 }
 
-int main(){
-
+int main() {
+    
     return 0;
 }
