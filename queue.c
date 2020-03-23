698edf608dc2a26eb3e08a41412a68ce6812e350
xuxin-to-future <51843322+xuxin-to-future@users.noreply.github.com>
2020-03-23 10:55:16 +0800

    队列

--- queue.c
+++ queue.c
@@ -1,56 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ERROR 0
-#define OK 1
-
-typedef struct Queue{
+typedef struct Queue {
     int *data;
-    int head, tail, length;
-}Queue;
+    int head, tail, size, cnt;
+} Queue;
 
-void init(Queue *q, int length) {
-    q->data = (int *)malloc(sizeof(int) * length);
-    q->head = 0;
-    q->tail = -1;
-    q->length = length;
-}
-
-int push(Queue *q, int element) {
-    if(q->tail + 1 >= q->length){
-		return ERROR;
-    }
-    q->tail++;
-    q->data[q->tail] = element;
-    return OK;
-}
-void output(Queue *q) {
-    for(int i = q->head; i <= q->tail; i++){
-        if(i == q->head){
-			printf("%d", q->data[i]);
-        }else{
-            printf(" %d", q->data[i]);
-        }
-    }
-    printf("\n");
-}
-int front(Queue *q) {
-    return q->data[q->head];
-}
-void pop(Queue *q) {
-    q->head++;
+Queue *init(int n) {
+    Queue *q = (Queue *)malloc(sizeof(Queue));
+    q->data = (int *)malloc(sizeof(int) * n);
+    q->head = q->tail = q->cnt = 0;
+    q->size = n;
+    return q;
 }
 
 int empty(Queue *q) {
-    return q->head > q->tail;
+    return q->cnt == 0;
+}
+
+int front(Queue *q) {
+    return q->data[q->head];
+}
+
+int push(Queue *q, int val) {
+    if (q == NULL) return 0;
+    if (q->cnt == q->size) return 0;
+    q->data[q->tail++] = val;
+    if (q->tail == q->size) q->tail -= q->size;
+    q->cnt += 1;
+    return 1;
+}
+
+int pop(Queue *q) {
+    if (q == NULL) return 0;
+    if (empty(q)) return 0;
+    q->head += 1;
+    if (q->head == q->size) q->head -= q->size;
+    q->cnt -= 1;
+    return 1;
+}
+
+void output(Queue *q) {
+    for (int i = q->head, j = 0; j < q->cnt; j++) {
+        printf("%d ", q->data[(i + j) % (q->size)]);
+    }
+    printf("]\n");
+    return ;
 }
 
 void clear(Queue *q) {
+    if (q == NULL) return ;
     free(q->data);
     free(q);
+    return ;
 }
 
 int main() {
-   
+    
     return 0;
 }
